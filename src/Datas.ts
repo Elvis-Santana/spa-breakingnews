bcd3f5b1f042098282663ad41808cbd359418abc
Elvis Santana <elvissantanaguimaraes@gmail.com>
2023-11-12 14:03:30 -0300

    feat: create data mock

---
+++ src/Datas.ts
@@ -0,0 +1,24 @@
+export const news =[
+    {
+        title:'Noticia 1',
+        text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, sint cupiditate vero quidem aliquid delectus sapiente praesentium accusantium ipsum recusandae repudiandae, dolore eveniet at temporibus facilis quaerat et ab labore.',
+        image:'https://avatars.githubusercontent.com/u/130992999?s=400&v=4',
+        likes: 2,
+        comments:3,
+    },
+    {
+        title:'Noticia 2',
+        text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, sint cupiditate vero quidem aliquid delectus sapiente praesentium accusantium ipsum recusandae repudiandae, dolore eveniet at temporibus facilis quaerat et ab labore.',
+        image:'https://avatars.githubusercontent.com/u/130992999?s=400&v=4',
+        likes: 2,
+        comments:3,
+    },
+    {
+        title:'Noticia 4',
+        text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, sint cupiditate vero quidem aliquid delectus sapiente praesentium accusantium ipsum recusandae repudiandae, dolore eveniet at temporibus facilis quaerat et ab labore.',
+        image:'https://avatars.githubusercontent.com/u/130992999?s=400&v=4',
+        likes: 2,
+        comments:3,
+    }
+]
+
